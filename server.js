bdb5a22cddef6a6db4722ac24b1c4f850079440d
deyperfect <edrylpalinis@gmail.com>
2026-03-03 07:50:45 +0800

    Adjusted cors

--- server.js
+++ server.js
@@ -16,9 +16,15 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: [
+    "http://localhost:5173",
+    process.env.CLIENT_URL,
+  ],
   credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 }));
+app.options("*", cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
